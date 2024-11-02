a9c98df8dd63156ef1cc12c5ee28ff9dc4760e5f
Eric Adamski <eric@zurp.com>
2024-11-02 09:48:05 -0400

    Update cli to v0.1.0-beta.13

--- Formula/remote.console.rb
+++ Formula/remote.console.rb
@@ -1,8 +1,8 @@
 class RemoteConsole < Formula
   desc "The official remote.console CLI for sending your console output to the clouds"
   homepage "https://github.com/ericadamski"
-  url "https://registry.npmjs.org/@remote.console/cli/-/cli-0.1.0-beta.12.tgz"
-  sha256 "be58bd17c0f2f6a05e1de589d89beb36c25cd5bad614a9e678da7b0c41b0b70f"
+  url "https://registry.npmjs.org/@remote.console/cli/-/cli-0.1.0-beta.13.tgz"
+  sha256 "e401b0093b2eec6d272544a4ab1f9d46927b2f2ebf56c4854e9de1ffe23f6471"
 
   # curl -o pkg.tgz $(npm view @remote.console/cli dist.tarball)
   # shasum -a 256 pkg.tgz; rm -rf pkg.tgz
